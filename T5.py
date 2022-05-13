8c593b7297e0ef5bde1572900b6c9da80beeb56e
Albert Vagapov <va3@list.ru>
2022-05-13 13:57:58 +0500

    HW2_1 done

---
+++ T5.py
@@ -0,0 +1,8 @@
+# str_1 = "2*x^2 + 4*x + 5".split(' ')
+# str_2 = "3x^2 + 5*x + 0".split(' ')
+# print(str_1)
+# print(str_2)
+
+# a1 = str_1[0].split('*')
+# a2 = str_2[0].split('*')
+# print(a1, a2)
